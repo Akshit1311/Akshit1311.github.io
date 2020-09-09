5faa7b026c0c5c4dff85974aaf00898a865012e2
Akshit1311 <a4748g@gmail.com>
2020-09-09 22:30:17 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const textPurple = 0xcf3da4;
+const textOrange = 0xf27611;
+const textGreen = 0x25b303;
+
+VANTA.NET({
+  el: "#vanta-net",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: textGreen,
+  backgroundColor: 0x242322,
+});
